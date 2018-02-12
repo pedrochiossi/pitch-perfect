3f92c8d343dd1cd88607d04aa02c240f8abcfef1
pedrochiossi <36369921+pedrochiossi@users.noreply.github.com>
2018-02-12 14:48:02 -0200

    Update PlaySoundsViewController.swift
    removed memory warning method

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -74,11 +74,7 @@ class PlaySoundsViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+  
 
     /*
     // MARK: - Navigation
